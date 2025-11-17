2b60cbf5a6540460bda51a7bec723a4b9d3c4bf9
PedroP57xd <pedropan2906@hotmail.com>
2025-11-17 12:57:41 -0500

    reparado

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import controllers.MetodoOrdenamiento;
 import controllers.PersonaController;
 import models.Persona;
 import views.ShowConsole;
+
 public class App {
     public static void main(String[] args) throws Exception {
         PersonaController pController = new PersonaController();
@@ -10,32 +11,26 @@ public class App {
         MetodoOrdenamiento sort = new MetodoOrdenamiento();
         BusquedaBinaria bBinaria = new BusquedaBinaria();
 
-
-        Persona[] people = new Persona[]{
-            new Persona("Juan", 25),
-            new Persona("Ana", 30),
-            new Persona("Pedro", 20),
-            new Persona("Maria", 28),
-            new Persona("Luis", 22),
-            new Persona("Carmen", 27),
-            new Persona("Sofia", 24),
+        Persona[] people = new Persona[] {
+                new Persona("Juan", 25),
+                new Persona("Ana", 30),
+                new Persona("Pedro", 20),
+                new Persona("Maria", 28),
+                new Persona("Luis", 22),
+                new Persona("Carmen", 27),
+                new Persona("Sofia", 24),
         };
 
         vista.showPersonas(people);
-        //ascendente
+        // ascendente
         sort.sort(people, true);
-        //descendente
+        // descendente
         // sort.sort(people, false);
         System.out.println();
         vista.showPersonas(people);
 
         Persona respBBinaria = bBinaria.busquedaByName(people, "Pedro");
         vista.showPersonaResult(respBBinaria, "Pedro");
-        Persona respBBinaria = bBinaria.busquedaByage(people, 20);
-        vista.showPersonaResult(respBBinaria, 20;
-        
 
-
-        
-    } 
+    }
 }
